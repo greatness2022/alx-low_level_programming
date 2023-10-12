68827df24f9e0177abbc1f82e9ac55a5e3cefcbd
greatness2022 <miracleonyenwe2586@gmail.com>
2023-10-12 13:10:25 -0700

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -3,23 +3,21 @@
 /**
  * print_square - prints a square of a given size using #
  * @size: size of the square
+ * Return: void
  */
 
 void print_square(int size)
 {
 	if (size <= 0)
-	{
 		_putchar('\n');
-	}
 	else
 	{
 		int i, j;
-		for (i = 0; i < size; i++)
+
+		for (i = 1 ; i <= size ; i++)
 		{
-			for (j = 0; j < size; j++)
-			{
+			for (j = 1 ; j <= size ; j++)
 				_putchar('#');
-			}
 			_putchar('\n');
 		}
 	}
