504d24bc266b072cb6302b60cb8051e11710be0b
greatness2022 <miracleonyenwe2586@gmail.com>
2023-10-18 18:30:52 -0700

    100-main.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rot13 - Encodes a string using ROT13 cipher.
+ * @str: The input string to be encoded.
+ *
+ * Return: A pointer to the encoded string.
+ */
+
+char *rot13(char *str)
+{
+	char *ptr = str;
+
+	while (*ptr != '\0')
+	{
+		if ((*ptr >= 'a' && *ptr <= 'z') || (*ptr >= 'A' && *ptr <= 'Z'))
+		{
+			char base = (*ptr >= 'a' && *ptr <= 'z') ? 'a' : 'A';
+			*ptr = ((*ptr - base + 13) % 26) + base;
+		}
+		ptr++;
+	}
+	return (str);
+}
