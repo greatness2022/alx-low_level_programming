3d340bdc62aa6d492b94fe800d3d0c6e78d9199f
greatness2022 <miracleonyenwe2586@gmail.com>
2023-10-19 16:02:45 -0700

    101-print_numbers

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -3,20 +3,21 @@
 /**
  * print_number - Prints an integer using _putchar function
  * @n: The integer to be printed
+ *
  */
 
 void print_number(int n)
 {
+	unsigned int i = n;
+
 	if (n < 0)
 	{
-		_putchar('-');
-		n = -n;
+		_putchar(45);
+		i = -i;
 	}
-
-	if (n / 10 != 0)
+	if (i / 10)
 	{
-		print_number(n / 10);
+		print_number(i / 10);
 	}
-
-	_putchar(n % 10 + '0');
+	_putchar(i % 10 + '0');
 }
