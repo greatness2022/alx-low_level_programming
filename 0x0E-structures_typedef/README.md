33165baddf0d0c1db04fbee2ab21c94c08c1ab51
greatness2022 <miracleonyenwe2586@gmail.com>
2023-11-06 13:12:11 -0800

    task

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,2 @@
+0x0E. C - Structures, typedef
+i want to code with this project task
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,24 +8,13 @@
  * @owner: owner of dog
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
-
-/**
- * dog_t - Typedef for struct dog
- */
-typedef struct dog dog_t;
-
-
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-char *_strcpy(char *dest, char *src);
-int _strlen(char *s);
 
 #endif
