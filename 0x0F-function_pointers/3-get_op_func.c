7206b87ffeef9a417694b5894f93e6ea6ba4ab6a
greatness2022 <miracleonyenwe2586@gmail.com>
2023-11-08 10:53:26 -0800

    Function pointers

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -20,12 +20,12 @@ int (*get_op_func(char *s))(int, int)
 		};
 	int i = 0;
 
-	while (i < 0)
+	while (i < 5)
 	{
 		if (strcmp(s, ops[i].op) == 0)
 			return (ops[i].f);
 
-		i++
+		i++;
 	}
 
 	return (0);
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -24,7 +24,7 @@ int main(int argc, char *argv[])
 	if (!oprt)
 	{
 		printf("Error\n");
-		exist(99);
+		exit(99);
 	}
 
 	printf("%d\n", oprt(atoi(argv[1]), atoi(argv[3])));
